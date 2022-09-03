45e37d0dc393fd8107a401920ac96c42e51a578e
sajiki <m.sajiki@secure-cycle.co.jp>
2022-09-03 16:18:53 +0900

    add create dir

---
+++ createdir.sh
@@ -0,0 +1,23 @@
+cd src
+mkdir api
+mkdir repositories
+mkdir usecase
+mkdir models
+mkdir store
+mkdir components
+mkdir pages
+mkdir routes
+mkdir pages
+mkdir theme
+mkdir __tests__
+cd __tests__
+mkdir api
+mkdir repositories
+mkdir usecase
+mkdir models
+# mkdir store
+mkdir components
+mkdir pages
+# mkdir routes
+mkdir pages
+# mkdir theme
